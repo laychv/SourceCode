af678dbb744ebfdd0ac9bebcfa5e93f639a4d7bf
ju <assess15@126.com>
2018-04-03 11:34:52 +0800

    add eventbus

--- app/src/main/java/cn/ju/sourcecode/MainActivity.kt
+++ app/src/main/java/cn/ju/sourcecode/MainActivity.kt
@@ -1,7 +1,10 @@
 package cn.ju.sourcecode
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import cn.ju.sourcecode.eventBus.EventBusActivity
+import cn.ju.sourcecode.okHttp.OkHttpActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.File
@@ -13,57 +16,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initClick()
+    }
 
-        // sync
-        btnSync.setOnClickListener {
-            syncRequest()
+    private fun initClick() {
+        // okHttp
+        okHttp.setOnClickListener {
+            startActivity(Intent(this, OkHttpActivity::class.java))
         }
-
-        // async
-        btnAsync.setOnClickListener {
-            asyncRequest()
+        // eventBus
+        eventBus.setOnClickListener {
+            startActivity(Intent(this, EventBusActivity::class.java))
         }
 
     }
 
-    // dispatcher 1.创建客户端
-    private val client = OkHttpClient.Builder()
-            .cache(Cache(File("file"), 24 * 1024 * 1024))// 缓存
-            .readTimeout(3, TimeUnit.SECONDS)
-            .build()
-
-    // 2.创建同步请求 阻塞线程
-    /**
-     * 同步 dispatcher 作用：1.添加同步请求，2.移除同步请求
-     */
-    private fun syncRequest() {
-        val request = Request.Builder().url("http://baidu.com").get().build()
-//        request.header()
-        // 3.创建桥梁
-        val call = client.newCall(request)// Call 是接口，通过实现类realCall实现
-        // 4.分水岭
-        try {
-            val response = call.execute()// 同步
-            println(response.body().toString())
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    // 2.创建异步请求
-    private fun asyncRequest() {
-        val request = Request.Builder().url("http://baidu.com").get().build()
-        val call = client.newCall(request)
-        call.enqueue(object : Callback { // 这里开启工作线程 // 异步
-            override fun onFailure(call: Call?, e: IOException?) {//都实在工作线程
-
-            }
-
-            override fun onResponse(call: Call?, response: Response?) {//都在工作线程操作 如果更新UI切换到主线程中
-                runOnUiThread {
-                    tvContent.text = response?.body().toString()
-                }
-            }
-        })
-    }
 }
---
+++ app/src/main/java/cn/ju/sourcecode/eventBus/EventBusActivity.kt
@@ -0,0 +1,15 @@
+package cn.ju.sourcecode.eventBus
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cn.ju.sourcecode.R
+
+class EventBusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_bus)
+    }
+
+
+}
