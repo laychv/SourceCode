f3b6f8ccfceddb486d75f9c439414b9cba778a51
Late <assess15@126.com>
2018-04-14 14:57:35 +0800

    add glide/picasso

--- app/src/main/java/cn/ju/sourcecode/MainActivity.kt
+++ app/src/main/java/cn/ju/sourcecode/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import cn.ju.sourcecode.eventBus.EventBusActivity
+import cn.ju.sourcecode.glide.GlideActivity
 import cn.ju.sourcecode.okHttp.OkHttpActivity
+import cn.ju.sourcecode.picasso.PicassoActivity
 import cn.ju.sourcecode.retrofit.RetrofitActivity
 import cn.ju.sourcecode.rxjava.RxJavaActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,13 +24,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initClick() {
-        // okHttp
-        okHttp.setOnClickListener {
-            startActivity(Intent(this, OkHttpActivity::class.java))
+        //Glide
+        glide.setOnClickListener {
+            startActivity(Intent(this, GlideActivity::class.java))
+        }
+        // Picasso
+        picasso.setOnClickListener{
+            startActivity(Intent(this,PicassoActivity::class.java))
         }
         // eventBus
         eventBus.setOnClickListener {
             startActivity(Intent(this, EventBusActivity::class.java))
+        }
+        // okHttp
+        okHttp.setOnClickListener {
+            startActivity(Intent(this, OkHttpActivity::class.java))
         }
         // Retrofit
         retrofit.setOnClickListener {
---
+++ app/src/main/java/cn/ju/sourcecode/glide/GlideActivity.kt
@@ -0,0 +1,12 @@
+package cn.ju.sourcecode.glide
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cn.ju.sourcecode.R
+
+class GlideActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_glide)
+    }
+}
