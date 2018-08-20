6adcd2e25ea8f4d167b15fdf062b5e6a7a0a5cb0
lpc <assess15@126.com>
2018-08-20 10:28:35 +0800

    update CurveView

--- app/src/main/java/cn/ju/cv/CVFragment.kt
+++ app/src/main/java/cn/ju/cv/CVFragment.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import cn.ju.comm.BaseFragment
 import cn.ju.cv.barChart.BarChartActivity
 import cn.ju.cv.codeView.ActivityCodeView
-import cn.ju.cv.cureView.CureViewActivity
+import cn.ju.cv.curveView.CurveViewActivity
 import cn.ju.cv.lineChart.LineChartActivity
 import cn.ju.sc.R
 import kotlinx.android.synthetic.main.fragment_cv.*
@@ -22,8 +22,9 @@ class CVFragment : BaseFragment() {
         // lineChart
         lineChart.setOnClickListener { startActivity(Intent(activity, LineChartActivity::class.java)) }
         // cureview
-        cureView.setOnClickListener { startActivity(Intent(activity, CureViewActivity::class.java)) }
-        codeView.setOnClickListener { startActivity(Intent(activity,ActivityCodeView::class.java)) }
+        cureView.setOnClickListener { startActivity(Intent(activity, CurveViewActivity::class.java)) }
+        codeView.setOnClickListener { startActivity(Intent(activity, ActivityCodeView::class.java)) }
+        cureView.setOnClickListener { startActivity(Intent(activity, CurveViewActivity::class.java)) }
     }
 
 }
